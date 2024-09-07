a8d02fc227de717f8fa1e141967eada54512cbe9
Manel Moura <emanuelmoura@gmail.com>
2024-09-07 00:33:49 -0300

    modified:   Diogenes.py

--- Diogenes.py
+++ Diogenes.py
@@ -214,8 +214,8 @@ async def generate_response_with_text(message_text):
 
 async def process_message(message):
     global sumario_global
-    if message.author == bot.user or message.mention_everyone or not message.author.bot:
-        return
+    
+    if message.author == bot.user or message.mention_everyone or (not message.author.bot and not isinstance(message.channel, discord.DMChannel)):        return
 
     if bot.user.mentioned_in(message) or isinstance(message.channel, discord.DMChannel):
         nome_usuario = message.author.name
